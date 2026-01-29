ff579d7d876b54e24a1470cfe401cbe2dac5ded8
Willams <williamekene700@gmail.com>
2026-01-29 16:22:33 +0100

    remove google-key.json to gemini api key for cloud run deployment

--- src/main/java/com/signal/Signal/config/GeminiConfig.java
+++ src/main/java/com/signal/Signal/config/GeminiConfig.java
@@ -1,33 +1,25 @@
 package com.signal.Signal.config;
 
-import com.google.auth.oauth2.GoogleCredentials;
 import com.google.genai.Client;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.Collections;
-
 @Configuration
 public class GeminiConfig {
 
-    @Value("${google.cloud.project-id}")
-    private String projectId;
+    @Value("${GOOGLE_API_KEY}")
+    private String apiKey;
 
     @Bean
-    public Client geminiClient() throws IOException {
-        String keyPath = "/etc/secrets/google-key.json";
+    public Client geminiClient() {
+        if (apiKey == null || apiKey.isEmpty()) {
+            throw new RuntimeException("GOOGLE_API_KEY is missing! Check your Cloud Run variables.");
+        }
 
-        GoogleCredentials credentials = GoogleCredentials.fromStream(new FileInputStream(keyPath))
-                .createScoped(Collections.singletonList("https://www.googleapis.com/auth/cloud-platform"));
 
         return Client.builder()
-                .project(projectId)
-                .location("global")
-                .credentials(credentials)
-                .vertexAI(true)
+                .apiKey(apiKey)
                 .build();
     }
 }
